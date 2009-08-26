d6c9222fe51c0250601bad7a7075fc5dac8ecf49
myerman <myerman@aad46a6a-76c8-11de-b8b8-6d0436ffb0fe>
2009-08-26 00:50:29 +0000

    adding a respond_id INT NULL to the messages table so we can keep track of responses!

--- trunk/system/application/models/m_messages.php
+++ trunk/system/application/models/m_messages.php
@@ -2,6 +2,7 @@
 /*
 CREATE TABLE `messages` (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`respond_id` INT NULL ,
 `from_id` INT NOT NULL ,
 `to_id` INT NOT NULL ,
 `subject` VARCHAR( 64 ) NOT NULL ,
