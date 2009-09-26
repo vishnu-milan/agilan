4f00c5dddf8298acc41ade909b647bd38c88e7d7
myerman <myerman@aad46a6a-76c8-11de-b8b8-6d0436ffb0fe>
2009-09-26 17:04:10 +0000

    added grid system for template, kept some stuff from leaves v1.0 css

--- system/application/views/agilan/sidebar1.php
+++ system/application/views/agilan/sidebar1.php
@@ -1,13 +1,9 @@
 
-		<ul id="navlist">
+		<ul class="localnav">
 			<li>Welcome <?php echo $user['firstname'];?></li>
-			<li><?php echo anchor("agilan/index",'home');?></li>
-			<li><?php echo anchor("messages/index","my messages");?></li>
 			<li><?php echo anchor("messages/compose","compose message");?></li>
 			<li><?php echo anchor("tags/index","my tags");?></li>
 			<li><?php echo anchor("bookmarks/index","my bookmarks");?></li>
-			<li><?php echo anchor("files/index", "file repository");?></li>
-			<li><?php echo anchor("blog/index", "blog");?></li>
 	
 			<p>We'll put messages here!</p>
 		</ul>
--- system/application/views/agilan/sidebar2.php
+++ system/application/views/agilan/sidebar2.php
@@ -1,17 +1,5 @@
-<?php
-echo heading("Search People",4);
-echo form_open('agilan/search');
-$input = array('name' => 'searchterm', 'id' => 'searchterm', 'size'=> 15);
-echo form_input($input);
-echo form_submit('go','go');
-echo form_close();
 
-?>
-
-<ul id="rightnav">
-<li><?php echo anchor("users/index", "see all users");?></li>
-<li><?php echo anchor("tags/all", "tag directory");?></li>
-<li><?php echo anchor("bookmarks/all", "bookmark directory");?></li>
+<ul class="localnav">
 <li><?php echo anchor("agilan/edit_profile",'edit profile');?></li>
 <li><?php echo anchor("agilan/logout",'logout');?></li>
 </ul>
