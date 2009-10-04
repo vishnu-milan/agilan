525524062ee3a14494d92429373b347aa64ef978
myerman <myerman@aad46a6a-76c8-11de-b8b8-6d0436ffb0fe>
2009-10-04 18:35:13 +0000

    added table view using CI table helper to inbox/archives/sent messages created a message detail view

--- trunk/system/application/views/agilan/inbox.php
+++ trunk/system/application/views/agilan/inbox.php
@@ -6,25 +6,33 @@ echo heading("Your Inbox",2);
 echo "inbox" . nbs(2) . "|" . nbs(2);
 echo anchor("messages/sent", "sent") . nbs(2) . "|" . nbs(2);
 echo anchor("messages/archive", "archives");
+$TABLE = array();
+
+//time format!
+$format = "%m/%d/%Y %h:%i %a";
+
 
 echo br(2);
 if (count($messages)){
 	foreach ($messages as $key => $msg){
-		echo "Subject: <b>". $msg->subject ."</b>";
-		echo br();
-		echo "From: ". $usernames[$msg->from_id];
-		echo br();
-		echo "To: ". $usernames[$msg->to_id];
-		echo br();
-		echo "Sent: ". $msg->created;
-		echo br();
-		echo auto_typography($msg->message);
-		echo br();
-		echo anchor("messages/respond/".$msg->id, "respond") . nbs(2) . "|" . nbs(2);
-		echo anchor("messages/archive_message/".$msg->id, "archive this"); 
-		echo "<hr/>";
-		echo br();
+		$stamp = mysql_to_unix($msg->created);
+		$TABLE[] = $usernames[$msg->from_id];
+		$TABLE[] =	$usernames[$msg->to_id];
+		$TABLE[] = anchor("messages/view_message/".$msg->id, $msg->subject);
+		$TABLE[] =  mdate($format,$stamp);
 	}
+
+
+	$tmpl = array (
+		'table_open'  => "<table class='messages'><tr valign='bottom'><th>From</th><th>To</th><th>Subject</th><th>Date/Time</th></tr>",
+		'row_start'   => "<tr valign='top'>",
+	);
+	$this->table->set_template($tmpl);
+	$this->table->set_empty("&nbsp;");
+	$pretty = $this->table->make_columns($TABLE,4);
+	
+	echo $this->table->generate($pretty);
+
 
 }else{
 	echo "No messages in inbox!";
