a425a5536dfd7882dc40b7516158a99f3275ff71
info@yorickpeterse.com <info@yorickpeterse.com@aad46a6a-76c8-11de-b8b8-6d0436ffb0fe>
2010-02-02 23:17:05 +0000

    So Subversion decided to cheat on me and act stupid, but I shall defeat this beast and rule the universe !

--- trunk/system/application/views/welcome/thanks.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo heading("Here are your login credentials....",2);
-echo "<p>Username: " . $_SESSION['username'] . "<br/>";
-echo "Password: " . $_SESSION['random_pw'] . "</p>";
-
-echo br(2);
-echo anchor("welcome/index", "Login now.");
-
-?>
