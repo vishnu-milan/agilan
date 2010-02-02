ec26d4443f1c89ab5fb5754273fa1c0e7b0e03c0
info <info@yorickpeterse.com>
2010-02-02 23:17:05 +0000

    So Subversion decided to cheat on me and act stupid, but I shall defeat this beast and rule the universe !

--- system/application/views/agilan/sidebar1.php
+++
@@ -1,8 +0,0 @@
-
-		<ul class="localnav">
-			<li><?php echo anchor("messages/compose","send message");?></li>
-			<li><?php echo anchor("tags/index","my tags");?></li>
-			<li><?php echo anchor("bookmarks/index","my bookmarks");?></li>
-	
-			<p>We'll put messages here!</p>
-		</ul>
--- system/application/views/welcome/oops.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo heading("There was a problem....",2);
-echo "<p>Sorry, looks like there was a problem of some kind. ";
-echo anchor("welcome/register", "Try again.");
-
-?>
